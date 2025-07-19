10f9543a43910423f0331417a2c78619a55157b5
jerrykoni <kerasimos@yahoo.gr>
2025-07-19 20:33:51 +0300

    Audio start on first frame, Fix OVRCameraRig rotation

---
+++ Assets/Scripts/Audio/SceneAwareAudioPlayer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneAwareAudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Awake()
+    {
+        // Ensure we don’t play automatically
+        audioSource.playOnAwake = false;
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Wait one frame so graphics have time to initialize
+        StartCoroutine(PlayAfterFrame());
+    }
+
+    private IEnumerator PlayAfterFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        audioSource.Play();
+    }
+}
